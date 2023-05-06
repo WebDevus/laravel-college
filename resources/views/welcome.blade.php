b83171ef3b823d68b5fff77de86321f2bb6fd510
Salman <salman03@yandex.ru>
2023-05-06 08:55:46 +0600

    Header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,39 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="/assets/css/main.css">
     <title>Магазин Сантехники</title>
   </head>
   <body>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <header id="glav">
+      <div class="col-lg-12 flex-wrap container-fluid">
+        <div class="row">
+          <div class="d-flex justify-content-center align-items-center">
+            <div class="d-flex justify-content-center align-items-center logo">
+              <img src="/assets/images/logo.png">
+            </div>
+            <div class="navigation">
+              <nav>
+                  <ul class="d-flex justify-content-between flex-wrap ul-class">
+                    <li><a href="">Каталог</a></li>
+                    <li><a href="">Корзина</a></li>
+                    <li><a href="">Где нас найти</h5></a></li>
+                    @auth
+                    <li><a href="#">Выйти</a></li>
+                    @endauth
+                    @guest
+                      <li><a href="#" class="header_button btn btn-info">Авторизация</a></li>
+                      <li><a href="#" class="header_button btn btn-info">Регистрация</a></li>             
+                    @endguest
+                  </ul>
+              </nav>
+            </div>
+            </div>
+          </div>
+        </div>
+      </header>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
